a5774955c8f32ca8fe2dbda41532bfeb2ef16609
Slava Nepran <slavanepran22@yandex.ru>
2022-04-15 13:44:56 +0300

    Пошел нахуй,Брат!!! Люблю тебя,Брат!!!

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, render_template
-
-from utils import create_words_file
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -12,20 +10,37 @@ def index_page():
         fio_2 = request.form.get('fio_2')
         fio_3 = request.form.get('fio_3')
         name_word_file = request.form.get('name_file')
-        create_words_file([fio_1, fio_2, fio_3], f'{name_word_file}')
-        return render_template('duty_table_3.html', tittle='Ответ сервера')
+        # create_words_file([fio_1, fio_2, fio_3], f'{name_word_file}')
+        return render_template('duty_table_regular.html', tittle='Ответ сервера')
     else:
         return render_template('index.html', title='СЭБ ОК РКЗ "Ресурс"')
 
 
-@app.route('/login')
+@app.route('/login/')
 def login_page():
     return render_template('login.html', title='Войти в систему')
 
 
-@app.route('/registration')
+@app.route('/registration/')
 def reg_page():
     return render_template('registration.html', title='Регистрация')
+
+
+@app.route('/irregular_graph/', methods=['GET', 'POST'])
+def irr_graph():
+    if request.method == 'POST':
+        input_count = int(request.form.get('input_count'))
+        return redirect(url_for('irr_graph_for_count_employees', count=input_count))
+    else:
+        return render_template('employees_count.html', title='СЭБ ОК РКЗ "Ресурс"')
+
+
+@app.route('/irregular_graph/employees/<int:count>/', methods=['GET', 'POST'])
+def irr_graph_for_count_employees(count):
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('duty_table_irregular.html', count_input=count, title='Ненормированный график')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,56 +1,39 @@
 import calendar
 import datetime
 
-from docx import Document
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-
 
 def show_duty_days(fio: list):
     duty_table = dict()
     days_list_temp = list()
     start_day = 1
-
     days_count = calendar.mdays[datetime.date.today().month]
 
     for name in fio:
-        days_list_temp.append(name)
         for day in range(start_day, days_count + 1, 3):
             days_list_temp.append(day)
-        duty_table[start_day] = days_list_temp
         start_day += 1
+        duty_table[name] = days_list_temp
         days_list_temp = []
 
     return duty_table
 
 
-def create_words_file(lst, name_file):
-    # создание пустого документа
-    doc = Document()
-    # данные таблицы без названий колонок
-    items = show_duty_days(lst)
-    # добавляем таблицу с одной строкой
-    # для заполнения названий колонок
-    table = doc.add_table(1, len(items[1]))
-    # определяем стиль таблицы
-    table.style = 'Table Grid'
-    # Получаем строку с колонками из добавленной таблицы
-    head_cells = table.rows[0].cells
-    # добавляем названия колонок
-    for i, item in enumerate(['ФИО', 'Даты дежурств']):
-        p = head_cells[i].paragraphs[0]
-        # название колонки
-        p.add_run(item).bold = True
-        # выравниваем посередине
-        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-    # добавляем данные к существующей таблице
-    for row in items.values():
-        # добавляем строку с ячейками к объекту таблицы
-        cells = table.add_row().cells
-        for i, item in enumerate(row):
-            # вставляем данные в ячейки
-            cells[i].text = str(item)
-            # если последняя ячейка
-            if i == 2:
-                # изменим шрифт
-                cells[i].paragraphs[0].runs[0].font.name = 'Arial'
-    doc.save(f'static/word_files/{name_file}.docx')
+def get_work_times(fio: list):
+    full_time = list()
+    night_time = list()
+    result_times_dict = dict()
+    for k, v in show_duty_days(fio).items():
+        for day in range(v[-1]):
+            if day in v:
+                full_time.insert(day, 22)
+                night_time.insert(day, 8)
+            else:
+                full_time.insert(day, '')
+                night_time.insert(day, '')
+            result_times_dict[k] = {'day': full_time, 'night': night_time}
+        full_time = []
+        night_time = []
+
+    return result_times_dict
+
+
